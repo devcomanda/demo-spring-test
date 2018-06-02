3dbf4d52b15a4b351213bdce337a5c6d2851ab99
Danil Kuznetsov <kuznetsov.danil.v@gmail.com>
2018-06-02 21:17:30 +0300

    initial project structure

---
+++ src/main/java/com/devcomanda/demotests/DemoSpringTestsApplication.java
@@ -0,0 +1,12 @@
+package com.devcomanda.demotests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringTestsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/devcomanda/demotests/springboottest/DemoSpringTestsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devcomanda.demotests.springboottest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringTestsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
