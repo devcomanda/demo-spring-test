c95d3aa52b5096cf8ec24dfaba81ea99898a9a71
Danil Kuznetsov <kuznetsov.danil.v@gmail.com>
2018-06-03 08:52:55 +0300

    fix stratup configuration

---
+++ src/main/java/com/devcomanda/demotests/config/DatabaseConfiguration.java
@@ -0,0 +1,11 @@
+package com.devcomanda.demotests.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("com.devcomanda.demotests.repository")
+public class DatabaseConfiguration {
+}
--- src/test/java/com/devcomanda/demotests/springboottest/DemoSpringTestsApplicationTests.java
+++ src/test/java/com/devcomanda/demotests/springboottest/DemoSpringTestsApplicationTests.java
@@ -2,11 +2,9 @@ package com.devcomanda.demotests.springboottest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoSpringTestsApplicationTests {
 
     @Test
